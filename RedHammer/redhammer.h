b2ffb4d92bd2b15fb20f0a6aa406d7f854b213cd
Jefferson <jeff.ssa@proton.me>
2026-09-20 22:22:56 -0300

    Normalize ROM pointer declarations

--- RedHammer/redhammer.h
+++ RedHammer/redhammer.h
@@ -26,12 +26,9 @@ void load_cps_roms(void);
 void *RHCodePtr(u32 offset);
 void *RHCodePtrChecked(u32 offset, const char *file, int line);
 void *RHCodePtrRange(u32 offset, size_t size);
-
 const void *RHOffsetLookup16(const u16 *base, int index);
-
 const u16 RHWordOffset(u32 base, int index);
 const u8 RHByteOffset(u32 base, int index);
-
 const u32 RH3DLong(u32 base, int dim2, int dim3, int i1, int i2, int i3);
 const u32 RH2DLong(u32 base, int dim2, int i1, int i2);
 const u32 RH1DLong(u32 base, int index);
@@ -41,26 +38,16 @@ const u16 RH2DWord(u32 base, int dim2, int i1, int i2);
 const short RH2DShort(u32 base, int dim2, int i1, int i2);
 const u8 RH3DByte(u32 base, int dim2, int dim3, int i1, int i2, int i3);
 const u8 RH2DByte(u32 base, int dim2, int i1, int i2);
-
-
 u32 RHSwapLong(const u32 num);
 u16 RHSwapWord(const u16 num);
-
 u32 RHReadLong(int romaddr);
 u16 RHReadWord(int romaddr);
-
 void print_rom_offset(const char *message, const void *addr);
-
 typedef u32 RHROMPtr;
 typedef u16 RHShortPtr;
-
 #define RHCODE(x) RHCodePtrChecked((u32)(x), __FILE__, __LINE__)
-
 #define RHCODE16(x) ((u16 *)RHCodePtrChecked((u32)(x), __FILE__, __LINE__))
-
 #define RHCODE16_ARRAY(base, stride, index) ((u16 *)RHCodePtrChecked((u32)(base) + 2u * (u32)(stride) * (u32)(index), __FILE__, __LINE__))
-
 void redhammer_run_tests(void);
-
 
 #endif
