8504f11e9229abe784b4f2c029310493462c60c7
Jefferson <jeff.ssa@proton.me>
2026-09-20 22:54:39 -0300

    Validate complete ROM word access

--- RedHammer/redhammer.h
+++ RedHammer/redhammer.h
@@ -50,7 +50,7 @@ typedef u32 RHROMPtr;
 typedef u16 RHShortPtr;
 #define RHCODE(x) RHCodePtrChecked((u32)(x), __FILE__, __LINE__)
 #define RHCODE_OFFSET(x, size) RHCodeOffsetChecked((x), (size), __FILE__, __LINE__)
-#define RHCODE16(x) ((u16 *)RHCodePtrChecked((u32)(x), __FILE__, __LINE__))
+#define RHCODE16(x) ((u16 *)RHCodePtrRange((u32)(x), sizeof(u16)))
 #define RHCODE16_ARRAY(base, stride, index) ((u16 *)RHCodePtrRange((u32)(base) + 2u * (u32)(stride) * (u32)(index), (size_t)(stride) * sizeof(u16)))
 void redhammer_run_tests(void);
 
