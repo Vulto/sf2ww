512fe67d9af4afe14508813a140e8d6e022907bb
Jefferson <jeff.ssa@proton.me>
2026-09-20 22:36:42 -0300

    Add checked ROM pointer offset API

--- RedHammer/redhammer.h
+++ RedHammer/redhammer.h
@@ -11,6 +11,7 @@
 
 #include "sf2types.h"
 #include <stddef.h>
+#include <stdint.h>
 
 extern char *g_code_roms;
 
@@ -26,6 +27,8 @@ void load_cps_roms(void);
 void *RHCodePtr(u32 offset);
 void *RHCodePtrChecked(u32 offset, const char *file, int line);
 void *RHCodePtrRange(u32 offset, size_t size);
+u32 RHCodeOffsetChecked(const void *addr, size_t size, const char *file, int line);
+u16 RHReadWordPtr(const void *addr);
 const void *RHOffsetLookup16(const u16 *base, int index);
 const u16 RHWordOffset(u32 base, int index);
 const u8 RHByteOffset(u32 base, int index);
@@ -46,6 +49,7 @@ void print_rom_offset(const char *message, const void *addr);
 typedef u32 RHROMPtr;
 typedef u16 RHShortPtr;
 #define RHCODE(x) RHCodePtrChecked((u32)(x), __FILE__, __LINE__)
+#define RHCODE_OFFSET(x, size) RHCodeOffsetChecked((x), (size), __FILE__, __LINE__)
 #define RHCODE16(x) ((u16 *)RHCodePtrChecked((u32)(x), __FILE__, __LINE__))
 #define RHCODE16_ARRAY(base, stride, index) ((u16 *)RHCodePtrChecked((u32)(base) + 2u * (u32)(stride) * (u32)(index), __FILE__, __LINE__))
 void redhammer_run_tests(void);
