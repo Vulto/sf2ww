d39079d8f5fe37b7b323e6de75c995d995c56aea
Jefferson <jeff.ssa@proton.me>
2026-09-20 22:49:17 -0300

    Lock down sprite data layouts

--- FBTests/test_redhammer.c
+++ FBTests/test_redhammer.c
@@ -64,3 +64,19 @@ void test_FBAction_layout(void **state) {
     assert_int_equal(20, offsetof(FBAction, YOffset));
     assert_int_equal(24, sizeof(FBAction));
 }
+
+void test_FBSimpleAction_layout(void **state) {
+    assert_int_equal(0, offsetof(FBSimpleAction, Delay));
+    assert_int_equal(2, offsetof(FBSimpleAction, Flags));
+    assert_int_equal(4, offsetof(FBSimpleAction, Image));
+    assert_int_equal(8, sizeof(FBSimpleAction));
+}
+
+void test_FBImage_layout(void **state) {
+    assert_int_equal(0, offsetof(FBImage, TileCount));
+    assert_int_equal(2, offsetof(FBImage, Attr));
+    assert_int_equal(4, offsetof(FBImage, Dimensions));
+    assert_int_equal(6, offsetof(FBImage, OffsetX));
+    assert_int_equal(8, offsetof(FBImage, OffsetY));
+    assert_int_equal(10, offsetof(FBImage, Tiles));
+}
