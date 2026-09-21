c20df8bc76a4bf4de450c983d30d67dc4eac03f6
Jefferson <jeff.ssa@proton.me>
2026-09-20 22:49:27 -0300

    Run sprite layout tests

--- FBTests/main.c
+++ FBTests/main.c
@@ -29,6 +29,8 @@ int main(int argc, const char * argv[]) {
         cmocka_unit_test(test_RHCodeOffsetChecked),
         cmocka_unit_test(test_RHReadWordPtr),
         cmocka_unit_test(test_FBAction_layout),
+        cmocka_unit_test(test_FBSimpleAction_layout),
+        cmocka_unit_test(test_FBImage_layout),
         cmocka_unit_test(test_FIXED16_16_carry),
         cmocka_unit_test(test_FIXED8_8_carry),
         cmocka_unit_test(test_SET_VECTFP16),
