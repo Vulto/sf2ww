3f7f483aff451604fc2f6329bf3805ab925130b2
Jefferson <jeff.ssa@proton.me>
2026-09-20 22:59:50 -0300

    Remove game-header-only layout test hooks

--- FBTests/test_redhammer.h
+++ FBTests/test_redhammer.h
@@ -18,7 +18,5 @@ void test_RHCODE(void **state);
 void test_RHCodeOffsetChecked(void **state);
 void test_RHReadWordPtr(void **state);
 void test_FBAction_layout(void **state);
-void test_FBSimpleAction_layout(void **state);
-void test_FBImage_layout(void **state);
 
 #endif /* test_redhammer_h */
