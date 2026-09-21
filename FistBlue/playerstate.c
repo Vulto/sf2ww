18c9be00d3b8edf02bc1d0912abc88e051ba18ff
Jefferson <jeff.ssa@proton.me>
2026-09-20 22:52:23 -0300

    Make player action metadata endian safe

--- FistBlue/playerstate.c
+++ FistBlue/playerstate.c
@@ -79,6 +79,25 @@ static short PSGetRoundResult(void) {		/* 2a768 */
 	return g.RoundResult;
 }
 
+static u8 player_action_extra_sprite(const Player *ply)
+{
+    if (ply->ActionScriptType == ACTIONSCRIPT_NATIVE) {
+        return ((const Action *)ply->ActionScript)->ExtraSprite;
+    }
+    return RHByteOffset(RHCODE_OFFSET(ply->ActionScript, sizeof(FBAction)),
+                        offsetof(FBAction, ExtraSprite));
+}
+
+static u8 player_action_priority(const Player *ply)
+{
+    if (ply->ActionScriptType == ACTIONSCRIPT_NATIVE) {
+        return ((const Action *)ply->ActionScript)->Priority;
+    }
+    return RHByteOffset(RHCODE_OFFSET(ply->ActionScript, sizeof(FBAction)),
+                        offsetof(FBAction, Priority));
+}
+
+
 void PSEntry(Player *ply) {   /* 0x28396 was: player_per_frame */
     short offsetsel;
 	
@@ -98,7 +117,7 @@ void PSEntry(Player *ply) {   /* 0x28396 was: player_per_frame */
 	// Update an 'extra' sprite associated with an avatar, such as Vega's claw
     ply->ExtraSpriteEna = FALSE;
     if(ply->exists && ply->VegaHasClaw) {
-        if((offsetsel = ply->ActionScript->ExtraSprite)) {
+        if((offsetsel = player_action_extra_sprite(ply))) {
             ply->ExtraSpriteEna = TRUE;
             ply->Draw_OffsetX = ply->Sprite2[offsetsel].Offset.x;
             ply->Draw_OffsetY = ply->Sprite2[offsetsel].Offset.y;
